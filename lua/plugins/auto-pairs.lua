d5cfca20db225dc3b694e5bf147fab41ac59c6c7
ljx <junxun-luo@outlook.com>
2024-02-05 22:01:45 +0800

    auto-pars

---
+++ lua/plugins/auto-pairs.lua
@@ -0,0 +1,26 @@
+-- 括号补全
+return{
+    {
+        "echasnovski/mini.pairs",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+          {
+            "<leader>up",
+            function()
+              local Util = require("lazy.core.util")
+              vim.g.minipairs_disable = not vim.g.minipairs_disable
+              if vim.g.minipairs_disable then
+                Util.warn("Disabled auto pairs", { title = "Option" })
+              else
+                Util.info("Enabled auto pairs", { title = "Option" })
+              end
+            end,
+            desc = "Toggle auto pairs",
+          },
+        }, 
+        config=function()
+          require("mini.pairs").setup(opts)
+        end
+      }
+}
