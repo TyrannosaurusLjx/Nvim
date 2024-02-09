83c62518861bdae59e4d9029050691fd253103fe
ljx <junxun-luo@outlook.com>
2024-02-09 13:40:23 +0800

    添加取消搜索映射

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,6 +4,9 @@ local keymap = vim.keymap
 -- insert mode
 keymap.set("i","qq","<ESC>")
 
+-- 正常模式
+keymap.set("n","//",":nohlsearch<CR>")  -- 使用 // 取消搜索
+
 -- 视觉模式下行移动
 keymap.set("v","J",":m '>+1<CR>gv=gv")
 keymap.set("v","K",":m '<-2<CR>gv=gv")
